d4ea66d8fb77c5b3b5fd87e185ed2161b4812bd3
Jonjo McKay <jonjo.mckay@manywho.com>
2015-08-21 09:40:43 +0100

    CORE-971: Added database schema

---
+++ Database/schema.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE storedjson
+(
+  id character varying(255) NOT NULL,
+  json jsonb NOT NULL,
+  createdat timestamp with time zone NOT NULL DEFAULT now(),
+  CONSTRAINT pk_storedjson PRIMARY KEY (id)
+);
