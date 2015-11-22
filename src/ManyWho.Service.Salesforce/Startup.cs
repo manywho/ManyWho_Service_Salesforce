4b992e72e9277321f3ada14c2a1a941954528f21
Jonjo McKay <jonjo.mckay@manywho.com>
2015-11-22 19:05:03 +0000

    Swapped order of configuration sources

--- src/ManyWho.Service.Salesforce/Startup.cs
+++ src/ManyWho.Service.Salesforce/Startup.cs
@@ -16,9 +16,9 @@ namespace ManyWho.Service.Salesforce
         public Startup(IHostingEnvironment env)
         {
             Configuration = new ConfigurationBuilder()
-                .AddEnvironmentVariables()
                 .AddIniFile($"config.{env.EnvironmentName.ToLower()}.ini")
-                .Build();   
+                .AddEnvironmentVariables()
+                .Build();
         }
 
         public void ConfigureServices(IServiceCollection services)
@@ -50,16 +50,14 @@ namespace ManyWho.Service.Salesforce
                 {
                     controller = "PluginSalesforce",
                     action = "Health"
-                }
-                );
+                });
 
                 // Email outcome click addition
                 routes.MapWebApiRoute("PluginSalesforceTaskEmailOutcomeResponse", "api/email/outcomeresponse", new
                 {
                     controller = "PluginSalesforce",
                     action = "TaskEmailOutcomeResponse"
-                }
-                );
+                });
 
                 // Salesforce Plugin
                 routes.MapWebApiRoute("PluginSalesforceWorkflowRuleListener", "plugins/api/salesforce/1/workflowrules/listener/{tenantId}",
@@ -82,232 +80,199 @@ namespace ManyWho.Service.Salesforce
                 {
                     controller = "PluginSalesforce",
                     action = "Canvas"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceSessionSignIn", "plugins/api/salesforce/1/session/{tenantId}/{flowId}/{playerUrl}", new
                 {
                     controller = "PluginSalesforce",
                     action = "SessionSignIn"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceDescribe", "plugins/api/salesforce/1/metadata", new
                 {
                     controller = "PluginSalesforce",
                     action = "Describe"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceDescribeTables", "plugins/api/salesforce/1/metadata/table", new
                 {
                     controller = "PluginSalesforce",
                     action = "DescribeTables"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceDescribeFields", "plugins/api/salesforce/1/metadata/field", new
                 {
                     controller = "PluginSalesforce",
                     action = "DescribeFields"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceView", "plugins/api/salesforce/1/view/{actionName}", new
                 {
                     controller = "PluginSalesforce",
                     action = "View"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceDelete", "plugins/api/salesforce/1/data/delete", new
                 {
                     controller = "PluginSalesforce",
                     action = "Delete"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceGetUserInAuthorizationContext", "plugins/api/salesforce/1/authorization", new
                 {
                     controller = "PluginSalesforce",
                     action = "GetUserInAuthorizationContext"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceLoadUserAttributes", "plugins/api/salesforce/1/authorization/user/attribute", new
                 {
                     controller = "PluginSalesforce",
                     action = "LoadUserAttributes"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceLoadGroupAttributes", "plugins/api/salesforce/1/authorization/group/attribute", new
                 {
                     controller = "PluginSalesforce",
                     action = "LoadGroupAttributes"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceLoadUsers", "plugins/api/salesforce/1/authorization/user", new
                 {
                     controller = "PluginSalesforce",
                     action = "LoadUsers"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceLoadGroups", "plugins/api/salesforce/1/authorization/group", new
                 {
                     controller = "PluginSalesforce",
                     action = "LoadGroups"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceLogin", "plugins/api/salesforce/1/authentication", new
                 {
                     controller = "PluginSalesforce",
                     action = "Login"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceVote", "plugins/api/salesforce/1/vote", new
-                    {
-                        controller = "PluginSalesforce",
-                        action = "Vote"
-                    }
-                );
+                {
+                    controller = "PluginSalesforce",
+                    action = "Vote"
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceListener", "plugins/api/salesforce/1/listener", new
-                    {
-                        controller = "PluginSalesforce",
-                        action = "Listener"
-                    }
-                );
+                {
+                    controller = "PluginSalesforce",
+                    action = "Listener"
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceNotification", "plugins/api/salesforce/1/notification", new
-                    {
-                        controller = "PluginSalesforce",
-                        action = "Notification"
-                    }
-                );
+                {
+                    controller = "PluginSalesforce",
+                    action = "Notification"
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceLoad", "plugins/api/salesforce/1/data", new
-                    {
-                        controller = "PluginSalesforce",
-                        action = "Load"
-                    }
-                );
+                {
+                    controller = "PluginSalesforce",
+                    action = "Load"
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceSave", "plugins/api/salesforce/1/data", new
-                    {
-                        controller = "PluginSalesforce",
-                        action = "Save"
-                    }
-                );
+                {
+                    controller = "PluginSalesforce",
+                    action = "Save"
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceLoadFiles", "plugins/api/salesforce/1/file", new
-                    {
-                        controller = "PluginSalesforce",
-                        action = "LoadFiles"
-                    }
-                );
+                {
+                    controller = "PluginSalesforce",
+                    action = "LoadFiles"
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceUploadFile", "plugins/api/salesforce/1/file/content", new
-                    {
-                        controller = "PluginSalesforce",
-                        action = "UploadFile"
-                    }
-                );
+                {
+                    controller = "PluginSalesforce",
+                    action = "UploadFile"
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceDeleteFile", "plugins/api/salesforce/1/file/delete", new
-                    {
-                        controller = "PluginSalesforce",
-                        action = "DeleteFile"
-                    }
-                );
+                {
+                    controller = "PluginSalesforce",
+                    action = "DeleteFile"
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceGetCurrentUserInfo", "plugins/api/salesforce/1/social/stream/{streamId}/user/me", new
-                    {
-                        controller = "PluginSalesforce",
-                        action = "GetCurrentUserInfo"
-                    }
-                );
+                {
+                    controller = "PluginSalesforce",
+                    action = "GetCurrentUserInfo"
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceSearchUsersByName", "plugins/api/salesforce/1/social/stream/{streamId}/user/name/{name}", new
                 {
                     controller = "PluginSalesforce",
                     action = "SearchUsersByName"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceGetUserInfo", "plugins/api/salesforce/1/social/stream/{streamId}/user/{userId}", new
-                    {
-                        controller = "PluginSalesforce",
-                        action = "GetUserInfo"
-                    }
-                );
+                {
+                    controller = "PluginSalesforce",
+                    action = "GetUserInfo"
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceGetStreamFollowers", "plugins/api/salesforce/1/social/stream/{streamId}/follower", new
                 {
                     controller = "PluginSalesforce",
                     action = "GetStreamFollowers"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceShareMessage", "plugins/api/salesforce/1/social/stream/{streamId}/share", new
                 {
                     controller = "PluginSalesforce",
                     action = "ShareMessage"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforcePostNewMessage", "plugins/api/salesforce/1/social/stream/{streamId}/message", new
                 {
                     controller = "PluginSalesforce",
                     action = "PostNewMessage"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceLikeMessage", "plugins/api/salesforce/1/social/stream/{streamId}/message/{messageId}/like/{like}", new
                 {
                     controller = "PluginSalesforce",
                     action = "LikeMessage"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceDeleteMessage", "plugins/api/salesforce/1/social/stream/{streamId}/message/{messageId}", new
                 {
                     controller = "Social",
                     action = "PluginSalesforce"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceFollowStream", "plugins/api/salesforce/1/social/stream/{streamId}/follow/{follow}", new
                 {
                     controller = "PluginSalesforce",
                     action = "FollowStream"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceCreateStream", "plugins/api/salesforce/1/social/stream", new
-                    {
-                        controller = "PluginSalesforce",
-                        action = "CreateStream"
-                    }
-                );
+                {
+                    controller = "PluginSalesforce",
+                    action = "CreateStream"
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceGetStreamMessages", "plugins/api/salesforce/1/social/stream/{streamId}", new
                 {
                     controller = "PluginSalesforce",
                     action = "GetStreamMessages"
-                }
-                );
+                });
 
                 routes.MapWebApiRoute("PluginSalesforceInvoke", "plugins/api/salesforce/1/{actionName}", new
                 {
                     controller = "PluginSalesforce",
                     action = "Invoke"
-                }
-                );
+                });
             });
         }
 
