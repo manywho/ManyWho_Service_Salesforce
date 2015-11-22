c47801f421193e98743a0508b1f27197b1951f9a
Jonjo McKay <jonjo.mckay@manywho.com>
2015-11-22 15:14:04 +0000

    Removed old files

--- Database/schema.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE storedjson
-(
-  id character varying(255) NOT NULL,
-  json jsonb NOT NULL,
-  createdat timestamp with time zone NOT NULL DEFAULT now(),
-  CONSTRAINT pk_storedjson PRIMARY KEY (id)
-);
--- Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ManyWho.Service.Salesforce.MvcApplication" Language="C#" %>
